618607f36df8f57173101b7786a2ad7750b0bb75
raviddog <raviddog@outlook.com>
2022-02-16 03:03:11 +1100

    test js rename node moduule

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const prompt = require("prompt-sync")({sigint:true});
-const thjson = require("./build/Debug/thjson.node");
+const threplay = require("./build/Debug/threplay.node");
 
 var filename = prompt("Enter replay filename to read:");
 fs.readFile(filename, function(err, data) {
     if(err) throw err;
-    console.log(thjson.get_replay_data(data));
+    console.log(threplay.get_replay_data(data));
 });
